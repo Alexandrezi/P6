563f43970a23a69ac54236ffb1e18f42da0ec782
Alexandrezi <85258183+Alexandrezi@users.noreply.github.com>
2021-06-24 14:02:44 +0200

    Add files via upload
    changement os.system vers subprocess.run

--- test1.py
+++ test1.py
@@ -1,12 +1,12 @@
 import requests
 import tarfile
 import subprocess
-import os
+
 
 glpiUrl='https://github.com/glpi-project/glpi/releases/download/9.5.2/glpi-9.5.2.tgz'
 downloadFile='/tmp/glpi-9.5.2.tgz'
 extractDir = '/tmp'
-Package = ["apache2", "php", "libapache2-mod-php", "mariadb-server", "php-mysqli", "php-mbstring", "php-curl", "php-gd", "php-simplexml", "php-intl", "php-ldap", "php-apcu", "php-xmlrpc", "php-cas", "php-zip", "php-bz2", "php-ldap", "php-imap"]                     
+Package = ["apache2", "php", "libapache2-mod-php", "mariadb-server", "php-mysqli", "php-mbstring", "php-curl", "php-gd", "php-simplexml", "php-intl", "php-ldap", "php-apcu", "php-xmlrpc", "php-cas", "php-zip", "php-bz2", "php-ldap", "php-imap"]
         
 def telechargement (url, download):
 #import requests
@@ -34,31 +34,24 @@ def installpackage (onepackagetoinstall):
         except:
                 print("erreur install package")
 
-#def mysqlinstall ():
-#	try:
-		os.system("mysql_secure_installation", shell=True)
-#		subprocess.call("y", shell=True)
-#	except:
-#		print("erreur5")
-
-#def mysqlinstall2 ():
-#	try:
-		os.system("mysql -u root -p=Alex0603!")
-#		subprocess.call("create database db_glpi;", shell=True)
-
+def mysqlinstall ():
+#installation base de données
+	try:
+		subprocess.run("mysql -e root -p=Alex0603!")
+	
+	except:
 
 def copie ():
 	try:
-		os.system("shopt -s dotglob")
-		os.system("rm /var/www/html/index.html")
-		os.system("cp -r /tmp/glpi/* /var/www/html")
-		os.system("chown -R www-data /var/www/html")
+		subprocess.run("rm /var/www/html/index.html")
+		subprocess.run("cp -r /tmp/glpi/* /var/www/html")
+		subprocess.run("chown -R www-data /var/www/html")
 	except:
 		print("erreur copie/droit") 
 
 def installglpi ():
 	try:
-		os.system("php /var/www/html/bin/console db:install --reconfigure --default-language=en_GB --db-name=db_glpi --db-user=admindb_glpi --db-password=MDP --force -n")
+		subprocess.run("php /var/www/html/bin/console db:install --reconfigure --default-language=en_GB --db-name=db_glpi --db-user=admindb_glpi --db-password=MDP --force -n")
 	except:
 		print("erreur installation glpi")
 
@@ -67,6 +60,6 @@ telechargement(glpiUrl, downloadFile)
 untar(downloadFile, extractDir)
 for onePackage in Package:
   installpackage(onePackage)
-#mysqlinstall()
+mysqlinstall()
 copie()
 installglpi()
